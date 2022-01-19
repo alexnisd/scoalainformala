0d9edc2ac75098ef52840e843087c7518e511233
unknown <decualex@gmail.com>
2022-01-19 21:29:14 +0200

    curs02

---
+++ 02-pythonic-hello-world/data_structures.py
@@ -0,0 +1,31 @@
+# lista = [8, 2, -5, "string", 2.7]
+# print(type(lista))
+# print(lista[3])
+# print(lista[3]) # daca indexul nu exista, apare eroare
+# print(lista[1:]) # afisam elementele de la indexul 1 inclusiv, pana la finalul listei
+# print(lista[1:3]) #
+# print(lista[5]) #
+# print(lista[0:5:2]) # elementele de la index 0 pana la indexul 5 (Exclus) din 2 in 2
+# denumire_lista[index_inceput:index_final-1:pas]
+# print(lista[-1]) # ultimul element din lista
+# print(lista[-1::-1]) # ultimul element din lista
+# lista.append("mere") # append adauga la final
+# lista[3] = "mere"
+# lista[5] = "mere"
+# print(len(lista)) # cate elemente sunt in lista
+# print(lista.index(-5))
+lista = [8, 2, -5, "string", 2.7]
+# lista.insert(3, "element nou")
+# lista.remove(-5) # scoate din lista remove face ref la denumirea elem
+# lista.pop(2) # face referire la indexu elementului
+# lista.clear() # lista goala
+# lista.reverse() # ordoneaza invers elementele din lista
+# lista.pop(3)
+# lista_stringuri = ['2', '8', '21', '18', '121', '191'] lista_stringuri.sort() print(lista_stringuri) Asa se înțelege mai clar
+lista_stringuri = ['8', '2', "21"]
+print(hex('8'), '8')
+print(ord('2'), '2')
+print(ord('1'), '21')
+lista_stringuri.sort()
+print(lista_stringuri)
+
---
+++ 02-pythonic-hello-world/main.py
@@ -0,0 +1,56 @@
+# print("Numele meu este Alex")
+# print(1, type(1)) # tipul int indif ca i cu plus sau minus tot int este
+# print(1.1, type(1.1)) # type float (rational / zecimal)
+# print(1j, type(1j)) # type complex
+# print(int(1.1))
+# print(float(1))
+# print(complex(1))
+# print(1+1) adunare
+# print(1-1) scadere
+# print(1*3) inmultire
+# print(1/2) impartirea exacta
+# print(1//2) catul
+# print(5%2) restul => modulo
+# print(5**2) ridicare la putere
+# print(5==2) operatorul de egalitate => returneaza un boolean (true sau false 0 sau 1)
+# print(5!=2) operatorul de inegalitate / diferentiere => returneaza un boolean (true sau false 0 sau 1)
+# print(5>2)
+# print(5<2)
+# print(5>=2)
+# print(5<=2)
+
+# print(5>2 and 3>1) operator logic si
+# print(5>2 or 3>1) operator logic sau
+# print(not(5>2 and 3>1)) operator de negare
+# print(7 is 7)
+# print(7 is not 7)
+
+# my_var = 0
+# my_var, my_var_1 =0, 1
+# my_var = 3
+# my_var = my_var + 1 asignare prin adunare / incrementare
+# my_var += 1
+# my_var *= 2 asignare prin inmultire
+# print(my_var)
+
+# print("Ana are mere")
+
+# variabila = "Ana `are` mere multe"
+# variabila = "Ana \'are\' \n mere"
+# variabila = """Ana are
+# mere
+# """
+# print(variabila)
+# nume = "Decu"
+# prenume = "Alexandru"
+# varsta = 19
+# inaltime = 19
+# prezentare = "Numele meu este {1} si prenumele meu este {0}".format(nume, prenume)
+# prezentare = f"Numele meu este {nume} si prenumele meu este {prenume}"
+# prezentare = "Numele meu este " + nume + " si prenumele este " + prenume + "si varsta mea este " + str(varsta) + "."
+# prezentare = "Numele meu este {0} si prenumele este {1} si varsta este {2}".format(nume, prenume, varsta)
+# prezentare = f"Numele meu este {nume} si prenumele este {prenume} si varsta este {varsta}"
+# print(prezentare)
+# calcul = inaltime + varsta
+# calcul = nume + prenume
+# print(calcul)
