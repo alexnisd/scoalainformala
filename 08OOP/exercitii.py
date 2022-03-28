c3b6a879f2739e078a03eae4c94483768a67a553
unknown <decualex@gmail.com>
2022-03-28 21:28:22 +0300

    mesaj

--- 08OOP/exercitii.py
+++ 08OOP/exercitii.py
@@ -166,27 +166,45 @@
 # print(obj2.culoare, obj2.blocuri_optice_led, obj2.marca, obj2.tip)
 # print(obj.culoare, obj.scaune_incalzite, obj.marca, obj.tip)
 
-class Calcul:
-    def __init__(self, a=1, b=2, c=3, d=4):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+# class Calcul:
+#     def __init__(self, a=1, b=2, c=3, d=4):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#         self.d = d
+#
+#     def verificare(self):
+#         self.e = f"Inf introduse nu sunt cifre"
+#         if str(self.a).isnumeric() and str(self.b).isnumeric() and str(self.c).isnumeric() and str(self.d).isnumeric():
+#             self.e = (self.a * (self.b + 3) / self.c) * self.d
+#         return self.e
+#
+#     def __str__(self):
+#         return f"Rezult. este: {self.verificare()}"
+#
+#
+# obiect = Calcul()
+# print(obiect)
+# obiect2 = Calcul(5, 6, 7, 8)
+# print(obiect2)
+# obiect3 = Calcul("x", "y", "z", 2)
+# print(obiect3)
+# obiect4 = Calcul(9, 2)
+# print(obiect4)
 
-    def verificare(self):
-        self.e = f"Inf introduse nu sunt cifre"
-        if str(self.a).isnumeric() and str(self.b).isnumeric() and str(self.c).isnumeric() and str(self.d).isnumeric():
-            self.e = (self.a * (self.b + 3) / self.c) * self.d
-        return self.e
 
-    def __str__(self):
-        return f"Rezult. este: {self.verificare()}"
+# class Lista_CD_DVD:
+#     lista_obj = []
+#
+#     def __init__(self, titlu, continut):
+#         self.titlu = titlu
+#         self.continut = continut
+#         self.lista_obj.append(self.titlu, self.continut)
+#
+#     @staticmethod
+#     def cautare(argument):
+#         msg = f"Obiectul nu a fost gasit"
+#         for i in Lista_CD_DVD.lista_obj:
+#             if argument in i[0] or argument in i[1]:
+#                 msg = f"Obiectul cautat este "
 
-obiect = Calcul()
-print(obiect)
-obiect2 = Calcul(5, 6, 7, 8)
-print(obiect2)
-obiect3 = Calcul("x", "y", "z", 2)
-print(obiect3)
-obiect4 = Calcul(9, 2)
-print(obiect4)
